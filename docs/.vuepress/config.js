6e17c447ddb2b9cafac4092ea574f4cbcee4c5e1
jiseongkwon <jskwon@directcloud.co.jp>
2019-02-15 19:53:56 +0900

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  base: "/TIL/",
+  title: 'VuePress 시작!',
+  description: 'Hello World',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' }
+    ]
+  }
+};
